aae4c576f7415e3a89aba402a8c29c580b980510
Ryan Kroon <rykroon.tech@gmail.com>
2022-10-13 21:53:04 -0400

    Added auto_now logic and some validation logic.

--- mongomini/query.py
+++ mongomini/query.py
@@ -1,8 +1,10 @@
 from dataclasses import asdict, fields
+from datetime import datetime
 from functools import cached_property
 
+from bson import ObjectId
+
 from .exceptions import ObjectDoesNotExist, MultipleObjectsReturned
-from .utils import Exclude
 
 
 class QueryManager:
@@ -18,6 +20,14 @@ class QueryManager:
     @cached_property
     def field_names(self):
         return [f.name for f in self.fields]
+
+    @cached_property
+    def _auto_now_fields(self):
+        return [f.name for f in self.fields if f.metadata.get('auto_now') is True]
+
+    @cached_property
+    def _auto_now_add_fields(self):
+        return [f.name for f in self.fields if f.metadata.get('auto_now_add') is True]
 
     def find(self, query):
         cursor = self.collection.find(query)
@@ -43,9 +53,12 @@ class QueryManager:
 
         return self._from_document(result)
 
-
     async def insert(self, obj, /):
-        document = asdict(obj, dict_factory=Exclude('_id'))
+        if obj._id is None:
+            obj._id = ObjectId()
+
+        self._set_auto_now_add_fields(obj)
+        document = asdict(obj)
         result = await self.collection.insert_one(document)
         assert result.acknowledged
         self._id = result.inserted_id
@@ -59,17 +72,43 @@ class QueryManager:
         assert result.modified_count == 1
 
     async def delete(self, obj, /):
-        result = await self.delete_by_id(obj._id)
+        return await self.delete_by_id(obj._id)
 
     async def delete_by_id(self, _id, /):
         query = {'_id': _id}
         result = await self.collection.delete_one(query)
         assert result.acknowledged
-        assert result.deleted_count == 1
+        return result.deleted_count
 
     def _from_document(self, document: dict):
         kwargs = {f: document[f] for f in self.field_names if f in document}
         return self.class_(**kwargs)
+
+    def _set_auto_now_add_fields(self, obj):
+        all_auto_now_fields = self._auto_now_add_fields + self._auto_now_fields
+        now = datetime.utcnow()
+        for field in all_auto_now_fields:
+            setattr(obj, field, now)
+
+    def _set_auto_now_fields(self, obj):
+        now = datetime.utcnow()
+        for field in self._auto_now_fields:
+            setattr(obj, field, now)
+
+    def _validate(self, obj):
+        for field in self.fields:
+            self._validate_field(field, obj)
+
+    def _validate_field(self, field, obj):
+        value = getattr(obj, field.name)
+
+        if not isinstance(value, field.type):
+            raise TypeError
+
+        choices = field.metadata.get('choices')
+        if choices is not None:
+            if value not in choices:
+                raise ValueError
 
 
 class ObjectIterator:
