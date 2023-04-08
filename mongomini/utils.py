eea70e3862f7ce5701b4ba579b4525fa1960f9ac
Ryan Kroon <rykroon.tech@gmail.com>
2023-04-08 10:17:43 -0400

    Changed the name of the inner functions.

--- mongomini/utils.py
+++ mongomini/utils.py
@@ -1,12 +1,12 @@
 
 
 def include(*fields: str):
-    def inner(iterable):
+    def include_dict_factory(iterable):
         return {k: v for k, v in iterable if k in fields}
-    return inner
+    return include_dict_factory
 
 
 def exclude(*fields):
-    def inner(iterable):
+    def exclude_dict_factory(iterable):
         return {k: v for k, v in iterable if k not in fields}
-    return inner
+    return exclude_dict_factory
