1bf19ff3d19fa15eaa1fb06203267b9a5df1eb89
Ryan Kroon <rykroon.tech@gmail.com>
2023-05-01 10:43:21 -0400

    Moved Document cursor to dataclasses.py

--- mongomini/dataclasses.py
+++ mongomini/dataclasses.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass, asdict, fields
+from functools import lru_cache
 
-from motor.motor_asyncio import AsyncIOMotorDatabase
-from .utils import include, get_init_field_names, get_non_init_field_names
+from motor.motor_asyncio import AsyncIOMotorDatabase, AsyncIOMotorCursor
+from pymongo import ASCENDING, DESCENDING
 
 
 _COLLECTION = '__mongomini_collection__'
@@ -34,12 +35,12 @@ def _process_class(cls, db, collection_name, repr, eq, order):
         for base in reversed(cls.mro()):
             if not is_documentclass(base):
                 continue
-            db = get_collection(base)
+            db = getattr(base, _COLLECTION)
 
     assert db is not None, "A database is required."
     collection_name = collection_name or cls.__name__.lower()
     collection = db[collection_name]
-    set_collection(cls, collection)
+    setattr(cls, _COLLECTION, collection)
     new_cls = dataclass(repr=repr, eq=eq, order=order, kw_only=True)(cls)
     assert any(f.name == '_id' for f in fields(new_cls)), "Missing '_id' field."
     return new_cls
@@ -50,72 +51,119 @@ def is_documentclass(obj):
     return hasattr(cls, _COLLECTION)
 
 
-def _is_document_instance(obj):
+def _is_documentclass_instance(obj):
     return hasattr(type(obj), _COLLECTION)
 
 
-def get_collection(obj):
-    return getattr(obj, _COLLECTION)
+def include(*fields: str):
+    def include_dict_factory(iterable):
+        return {k: v for k, v in iterable if k in fields}
+    return include_dict_factory
 
 
-def set_collection(obj, collection):
-    setattr(obj, _COLLECTION, collection)
+def exclude(*fields: str):
+    def exclude_dict_factory(iterable):
+        return {k: v for k, v in iterable if k not in fields}
+    return exclude_dict_factory
 
 
 async def insert_one(obj, /):
-    if not _is_document_instance(obj):
+    if not _is_documentclass_instance(obj):
         raise TypeError
 
     document = asdict(obj)
     if document['_id'] is None:
         del document['_id']
 
-    collection = get_collection(obj)
+    collection = getattr(obj, _COLLECTION)
     result = await collection.insert_one(document)
     obj._id = result.inserted_id
     return result
 
 
 async def update_one(obj, /, *fields):
-    if not _is_document_instance(obj):
+    if not _is_documentclass_instance(obj):
         raise TypeError
 
     dict_factory = include(*fields) if fields else dict
     document = asdict(obj, dict_factory=dict_factory)
-    collection = get_collection(obj)
+    collection = getattr(obj, _COLLECTION)
     return await collection.update_one(filter={"_id": obj._id}, update={"$set": document})
 
 
 async def delete_one(obj, /):
-    if not _is_document_instance(obj):
+    if not _is_documentclass_instance(obj):
         raise TypeError
 
-    collection = get_collection(obj)
+    collection = getattr(obj, _COLLECTION)
     return await collection.delete_one({"_id": obj._id})
 
 
 async def find_one(cls, query):
     if not is_documentclass(cls):
         raise TypeError
-    
-    collection = get_collection(cls)
+
+    collection = getattr(cls, _COLLECTION)
     return await collection.find_one(query)
 
 
 def find(cls, query):
-    ...
+    if not is_documentclass(cls):
+        raise TypeError
+    collection = getattr(cls, _COLLECTION)
+    cursor = collection.find(filter=query)
+    return DocumentCursor(cls, cursor)
+
+
+@lru_cache
+def _get_init_field_names(cls: type) -> list[str]:
+    return [f.name for f in fields(cls) if f.init]
+
+
+@lru_cache
+def _get_non_init_field_names(cls: type) -> list[str]:
+    return [f.name for f in fields(cls) if not f.init]
 
 
 def from_document(cls, document: dict):
     if not is_documentclass(cls):
         raise TypeError
 
-    init_kwargs = {f: document[f] for f in get_init_field_names(cls) if f in document}
+    init_kwargs = {f: document[f] for f in _get_init_field_names(cls) if f in document}
     obj = cls(**init_kwargs)
 
-    for f in get_non_init_field_names(cls):
+    for f in _get_non_init_field_names(cls):
         if f not in document:
             continue
         setattr(obj, f, document[f])
 
     return obj
+
+
+@dataclass(eq=False, slots=True, frozen=True)
+class DocumentCursor:
+
+    document_class: type
+    cursor: AsyncIOMotorCursor
+
+    def __aiter__(self):
+        return self
+
+    async def __anext__(self):
+        document = await anext(self.cursor)
+        return from_document(self.document_class, document)
+
+    def limit(self, limit: int):
+        self.cursor.limit(limit)
+
+    def skip(self, skip: int):
+        self.cursor.skip(skip)
+
+    def sort(self, *fields: str):
+        field_list = [
+            (f, ASCENDING)
+            if not f.startswith('-')
+            else (f[1:], DESCENDING)
+            for f in fields
+        ]
+        self.cursor.sort(field_list)
