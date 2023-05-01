028cc81aefa2f544dab7746d595da2abe041ae9a
Ryan Kroon <rykroon.tech@gmail.com>
2023-05-01 16:08:13 -0400

    added some ideas as comments.

--- mongomini/__about__.py
+++ mongomini/__about__.py
@@ -1,1 +1,12 @@
 __version__ = '0.3.0'
+
+"""
+    Ideas
+
+    - Add result cache  to Document Cursor.
+    - Add more tests.
+    - Add testing for python 3.10
+    - Add docstrings to functions.
+    - Add support for sync?
+
+"""
